cd9ff8bfba96d944efe7bbe4db91441126b98f97
Patrick Sollars <pjsollars@gmail.com>
2019-04-09 20:29:44 -0400

    Add Patrick's facts

--- src/Facts.js
+++ src/Facts.js
@@ -1,7 +1,10 @@
 const facts = [
   "JavaScript was created in 1995 by Brandon Eich while working at Netscape.",
   "The Python programming language's name is derived from the British comedy group Monty Python.",
-  "React is a user interface library written in JavaScript. It is maintained by Facebook and a community of independent developers."
+  "React is a user interface library written in JavaScript. It is maintained by Facebook and a community of independent developers.",
+  "Patrick is an endurance runner, completing a marathon and several 25k Riverbank runs.",
+  "Patrick has a brindle pitbull named Benji, who has one floppy ear.",
+  "Patrick's favorite style of beer is German hefeweizen."
 ];
 
 export default facts;
