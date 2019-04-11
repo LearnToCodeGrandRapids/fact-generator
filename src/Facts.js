4b2c1d3cd06bbf09b73c15e1fad309b6f3d7cbc9
Mark Haines <hainesma@gmail.com>
2019-04-11 19:17:11 -0400

    added mark fact

--- src/Facts.js
+++ src/Facts.js
@@ -1,7 +1,8 @@
 const facts = [
   "JavaScript was created in 1995 by Brandon Eich while working at Netscape.",
   "The Python programming language's name is derived from the British comedy group Monty Python.",
-  "React is a user interface library written in JavaScript. It is maintained by Facebook and a community of independent developers."
+  "React is a user interface library written in JavaScript. It is maintained by Facebook and a community of independent developers.",
+  "Mark is really cool."
 ];
 
 export default facts;
